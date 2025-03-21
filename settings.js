858100fdb5d3e53dbe005a8a429b607b550b48eb
DARKOFC009 <84442326+nikhilarma@users.noreply.github.com>
2025-03-21 14:04:04 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,13 +1,13 @@
 //auto presence update
-global.bankname = "MONIEPOINT"
+global.bankname = "❝𓂃❤️✨⃝͢ᴄʀɪᴍɪɴᴀʟ✨⃝🌎𓂃❞«⁩"
 
-global.banknumber = "8133729715"
+global.banknumber = "573226787103"
 
-global.bankowner = "FADARE"
+global.bankowner = ".»❝𓂃❤️✨⃝͢ᴄʀɪᴍɪɴᴀʟ✨⃝🌎𓂃❞«⁩"
 
 global.autoTyping = false //auto tying in gc (true to on, false to off)
 global.autoRecord = false //auto recording (true to on, false to off)
-global.wlcm = true
+global.wlcm = false
 global.autoblockmorroco = true //auto block 212 (true to on, false to off)
 global.autokickmorroco = true //auto kick 212 (true to on, false to off) 
 global.antispam = false//auto kick spammer (true to on, false to off)
